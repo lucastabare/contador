6b3064760fba1f5b329e377a8aaca6967d6f3c2d
Lucas Tabare <lucas.tabare@gmail.com>
2022-05-09 14:37:47 -0300

    Agregando Estilos

--- src/componets/contador.js
+++ src/componets/contador.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import Box from "@mui/material/Box";
 
 function Contador() {
+  const MySwal = withReactContent(Swal);
   const [contador, setContador] = useState(0);
 
   const aumentar = () => setContador(contador + 1);
@@ -8,15 +13,41 @@ function Contador() {
     if (contador >= 1) {
       setContador(contador - 1);
     } else {
-      alert("Esto es un contador, no se puede contar menos que CERO");
+      MySwal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "No se pueden sumar numeros negativos",
+        footer: "Intente nuevamente",
+      });
     }
   };
 
   return (
     <div>
-      <h1>Contador : {contador}</h1>
-      <button onClick={aumentar}>+</button>
-      <button onClick={disminuir}>-</button>
+      <div className="central">
+        <h1 className={contador < 1 ? "menor" : "mayor"}>
+          Contador : {contador}
+        </h1>
+      </div>
+      <Box sx={{ "& button": { m: 1 } }}>
+        <Button
+          className="button"
+          size="large"
+          variant="contained"
+          color="error"
+          onClick={disminuir}
+        >
+          -
+        </Button>
+        <Button
+          size="large"
+          variant="contained"
+          color="success"
+          onClick={aumentar}
+        >
+          +
+        </Button>
+      </Box>
     </div>
   );
 }
