2589faaca34f0b4fc1712a1c5c4b6f6d26fe63be
Lucas Tabare <lucas.tabare@gmail.com>
2022-05-09 13:19:07 -0300

    Actualizando El Estado

--- src/componets/contador.js
+++ src/componets/contador.js
@@ -3,11 +3,20 @@ import React, { useState } from "react";
 function Contador() {
   const [contador, setContador] = useState(0);
 
+  const aumentar = () => setContador(contador + 1);
+  const disminuir = () => {
+    if (contador >= 1) {
+      setContador(contador - 1);
+    } else {
+      alert("Esto es un contador, no se puede contar menos que CERO");
+    }
+  };
+
   return (
     <div>
       <h1>Contador : {contador}</h1>
-      <button>+</button>
-      <button>-</button>
+      <button onClick={aumentar}>+</button>
+      <button onClick={disminuir}>-</button>
     </div>
   );
 }
